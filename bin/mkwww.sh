e4292a2cd1c5eea312e12e9f352615dcec5f9cbe
anjakefala <anja.kefala@gmail.com>
2021-02-28 22:14:56 -0800

    Add link_attributes to pandoc build, allow assigning attributes to links and images

--- bin/mkwww.sh
+++ bin/mkwww.sh
@@ -17,7 +17,7 @@ function build_page () {
     # $3 -> page title (quoted for shell to give as single argument)
     mkdir -p "$BUILD"/"$1"
     fnbody=$(mktemp)
-    pandoc --from markdown_strict+table_captions+header_attributes+implicit_header_references+simple_tables+fenced_code_blocks+pipe_tables+fenced_divs -w html -o "$fnbody" "$2"
+    pandoc --from markdown_strict+table_captions+header_attributes+implicit_header_references+simple_tables+fenced_code_blocks+pipe_tables+fenced_divs+link_attributes -w html -o "$fnbody" "$2"
     "$BIN"/strformat.py body="$fnbody" title="$3" head="" < "$WWWSRC"/template.html > "$BUILD"/"$1"/index.html
     rm "$fnbody"
 }
